56aa43a628afeae3d0f288469233686f8fab4953
yanadsl <yanadsl@users.noreply.github.com>
2017-07-29 23:25:38 +0800

    Update locker.py

--- locker.py
+++ locker.py
@@ -2,7 +2,9 @@ import time
 import os
 from ctypes import Structure, windll, c_uint, sizeof, byref
 
-DEVICE_IP = ["192.168.50.197"]
+DEVICE_IP = ["192.168.50.197", ]  # input devices IPs here
+CHECK_TIME = 15  # How often will the process run
+IDLE_TIME = 60  # Time of idling
 
 
 class LASTINPUTINFO(Structure):
@@ -26,20 +28,18 @@ SwitchDesktop = user32.SwitchDesktop
 DESKTOP_SWITCHDESKTOP = 0x0100
 
 while True:
-    time.sleep(15)
-    if get_idle_duration() < 60:  # user should be idle at least 1 min
+    time.sleep(CHECK_TIME)
+    if get_idle_duration() < IDLE_TIME:  # user should be idle at least 1 min to lock screen
         continue
-    print("Idleing")
 
     hDesktop = OpenDesktop("default", 0, False, DESKTOP_SWITCHDESKTOP)
     result = SwitchDesktop(hDesktop)
     if not result:  # screen locked already
         continue
 
-    print("scanning ip")
     all_offline = 1
     for IP in DEVICE_IP:
-        if not os.system("ping -n 1 -w 6000 " + IP):  # if device is offline 0=succeed
+        if not os.system("ping -n 1 -w 3000 " + IP):  # if device is offline 0=succeed
             all_offline = 0
-    if all_offline:
+    if all_offline:  # every device is offline
         windll.user32.LockWorkStation()
